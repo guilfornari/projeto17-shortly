bdec19143f019ef35e6db040d0b0f3a16d7f0e49
Guilfornari <guilfornari@gmail.com>
2023-05-20 15:14:16 -0300

    Implemented route to get url by id

--- src/controllers/url.controllers.js
+++ src/controllers/url.controllers.js
@@ -1,5 +1,5 @@
 import { db } from "../database/database.js";
-import { nanoid } from 'nanoid'
+import { nanoid } from "nanoid";
 
 export async function createUrl(req, res) {
     const { url } = req.body;
@@ -14,13 +14,35 @@ export async function createUrl(req, res) {
         const urlId = await db.query(`
         SELECT "shortenedUrls".id 
         FROM "shortenedUrls" 
-        WHERE "shortenedUrls"."shortUrl" = $1 `, [shortUrl])
+        WHERE "shortenedUrls"."shortUrl" = $1 `, [shortUrl]);
 
         const answer = { id: urlId.rows[0].id, shortUrl: shortUrl };
 
-        res.status(201).send(answer)
+        res.status(201).send(answer);
 
     } catch (err) {
         res.status(500).send(err.message);
     }
 }
+
+export async function getOneUrl(req, res) {
+    const { id } = req.params;
+
+    try {
+
+        const oneUrl = await db.query(`
+        SELECT "shortenedUrls"."shortUrl", "shortenedUrls".url
+        FROM "shortenedUrls" 
+        WHERE "shortenedUrls".id = $1 `, [id]);
+
+        if (oneUrl.rowCount === 0) return res.sendStatus(404);
+
+        const answer = { id: id, shortUrl: oneUrl.rows[0].shortUrl, url: oneUrl.rows[0].url };
+
+        res.status(200).send(answer);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/url.schema.js";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
-import { createUrl } from "../controllers/url.controllers.js";
+import { createUrl, getOneUrl } from "../controllers/url.controllers.js";
 
 const urlRouter = Router();
+
+urlRouter.get("/urls/:id", getOneUrl);
 
 urlRouter.use(authValidation);
 
