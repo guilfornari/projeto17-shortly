e59061d902f1205e267bb5b9f2c0e7d324714fb5
Guilfornari <guilfornari@gmail.com>
2023-05-20 12:36:38 -0300

    Implemeted route signIn

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import { db } from "../database/database.js";
 import bcrypt from "bcrypt";
-
+import { v4 as uuid } from "uuid";
 
 export async function signUp(req, res) {
     const { name, email, password, confirmPassword } = req.body;
@@ -9,7 +9,7 @@ export async function signUp(req, res) {
 
     try {
 
-        const user = await db.query(`SELECT users.email FROM users WHERE email = $1;`, [email]);
+        const user = await db.query(`SELECT users.email FROM users WHERE users.email = $1;`, [email]);
         if (user.rows[0]) return res.status(409).send("E-mail already registered");
 
         const hash = bcrypt.hashSync(password, 10);
@@ -24,3 +24,29 @@ export async function signUp(req, res) {
         res.status(500).send(err.message);
     }
 }
+
+export async function signIn(req, res) {
+    const { email, password } = req.body;
+
+    try {
+        const user = await db.query(`
+        SELECT
+            users.id, 
+            users.email, 
+            users.password 
+            FROM users WHERE users.email = $1`, [email]);
+
+        if (user.rowCount === 0) return res.status(401).send("E-mail not registered");
+        const checkPw = bcrypt.compareSync(password, user.rows[0].password);
+        if (!checkPw) return res.status(401).send("Incorrect password");
+
+        const token = uuid();
+        await db.query(`INSERT INTO sessions ("userId", "token") VALUES ($1, $2);`, [user.rows[0].id, token]);
+
+        return res.status(200).send({ token: token });
+
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
+
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
-import { singUpSchema } from "../schemas/auth.schema.js";
-import { signUp } from "../controllers/auth.controllers.js";
+import { signInSchema, singUpSchema } from "../schemas/auth.schema.js";
+import { signIn, signUp } from "../controllers/auth.controllers.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", validateSchema(singUpSchema), signUp);
-authRouter.post("/signin",);
+authRouter.post("/signin", validateSchema(signInSchema), signIn);
 
 export default authRouter;
