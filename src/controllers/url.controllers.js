2b9a9fe3b55df990c16cf2d1403188dc4dd0ab3d
Guilfornari <guilfornari@gmail.com>
2023-05-20 15:44:26 -0300

    Implemented route to redirect to url and add visitcount

--- src/controllers/url.controllers.js
+++ src/controllers/url.controllers.js
@@ -14,7 +14,7 @@ export async function createUrl(req, res) {
         const urlId = await db.query(`
         SELECT "shortenedUrls".id 
         FROM "shortenedUrls" 
-        WHERE "shortenedUrls"."shortUrl" = $1 `, [shortUrl]);
+        WHERE "shortenedUrls"."shortUrl" = $1;`, [shortUrl]);
 
         const answer = { id: urlId.rows[0].id, shortUrl: shortUrl };
 
@@ -33,13 +33,38 @@ export async function getOneUrl(req, res) {
         const oneUrl = await db.query(`
         SELECT "shortenedUrls"."shortUrl", "shortenedUrls".url
         FROM "shortenedUrls" 
-        WHERE "shortenedUrls".id = $1 `, [id]);
+        WHERE "shortenedUrls".id = $1;`, [id]);
 
         if (oneUrl.rowCount === 0) return res.sendStatus(404);
 
         const answer = { id: id, shortUrl: oneUrl.rows[0].shortUrl, url: oneUrl.rows[0].url };
 
         res.status(200).send(answer);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function redirectTo(req, res) {
+    const { shortUrl } = req.params;
+
+    try {
+        const oneUrl = await db.query(`
+        SELECT "shortenedUrls".url, "shortenedUrls"."visitCount"
+        FROM "shortenedUrls" 
+        WHERE "shortenedUrls"."shortUrl" = $1;`, [shortUrl]);
+
+        if (oneUrl.rowCount === 0) return res.sendStatus(404);
+
+        const addCount = oneUrl.rows[0].visitCount + 1;
+
+        await db.query(`
+        UPDATE "shortenedUrls"
+        SET "visitCount" = $1
+        WHERE "shortenedUrls"."shortUrl" = $2`, [addCount, shortUrl]);
+
+        res.redirect(oneUrl.rows[0].url);
 
     } catch (err) {
         res.status(500).send(err.message);
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/url.schema.js";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
-import { createUrl, getOneUrl } from "../controllers/url.controllers.js";
+import { createUrl, getOneUrl, redirectTo } from "../controllers/url.controllers.js";
 
 const urlRouter = Router();
 
 urlRouter.get("/urls/:id", getOneUrl);
+urlRouter.get("/urls/open/:shortUrl", redirectTo);
 
 urlRouter.use(authValidation);
 
